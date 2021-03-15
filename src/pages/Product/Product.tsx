2203d680b1e68cdf721ab58d851d94cb1513034c
Akhmed <ahmedkambaev@ccagency.ru>
2021-03-15 10:23:48 +0300

    fixed adaptive

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -105,42 +105,45 @@ const data = [
 const Product: FC<IProps> = () => {
   return (
     <div className="page-with-header">
-      <div className="container pt-3">
-        <Breadcrumbs />
-        <h1 className="font-weight">Карточка продукта</h1>
-        <div className="d-flex Product-card">
-          <img
-            className="Product-card--image"
-            src="https://img4.okidoker.com/c/6/8/8/588738/7259859/15540373_2.jpg"
-            alt="Product-card"
-          />
-          <div>
-            <h3 className="mb-0 Product-card--title">PBA032 PMC</h3>
-            <p className="color-gray mb-2 Product-card--subtitle">
-              ФИЛЬТР МАСЛЯНЫЙ (БЕНЗИН) SANAFE NEW (МОЖНО СТАВИТЬ 2630035503)
-            </p>
-            <em className="Product-card--description">
-              Южнокорейская компания Parts-Mall (Партс-Молл) Corporation была
-              основана в конце декабря 1998 года. Компания является
-              изготовителем и экспортером расходных материалов, деталей и
-              сопутствующих материалов высокого качества на конвейеры для всего
-              модельного ряд автомобилей Hyundai, Ssang Yong, Kia, GM Daewoo и
-              Samsung Motors.
-            </em>
-            <a className="ml-1" href="/" target="_blank">
-              подробнее
-            </a>
+      <div className="container pt-3 d-flex">
+        <div className="Product-column--left">
+          <Breadcrumbs />
+          <h1 className="font-weight">Карточка продукта</h1>
+          <div className="d-flex Product-card">
+            <img
+              className="Product-card--image"
+              src="https://img4.okidoker.com/c/6/8/8/588738/7259859/15540373_2.jpg"
+              alt="Product-card"
+            />
+            <div>
+              <h3 className="mb-0 Product-card--title">PBA032 PMC</h3>
+              <p className="color-gray mb-2 Product-card--subtitle">
+                ФИЛЬТР МАСЛЯНЫЙ (БЕНЗИН) SANAFE NEW (МОЖНО СТАВИТЬ 2630035503)
+              </p>
+              <em className="Product-card--description">
+                Южнокорейская компания Parts-Mall (Партс-Молл) Corporation была
+                основана в конце декабря 1998 года. Компания является
+                изготовителем и экспортером расходных материалов, деталей и
+                сопутствующих материалов высокого качества на конвейеры для
+                всего модельного ряд автомобилей Hyundai, Ssang Yong, Kia, GM
+                Daewoo и Samsung Motors.
+              </em>
+              <a className="ml-1" href="/" target="_blank">
+                подробнее
+              </a>
+            </div>
           </div>
+          <h4 className="font-weight title-table">
+            Наличие для запрошенного артикула на центральном складе Кореана
+          </h4>
+          <Table data={data} columns={columns} />
+          <h4 className="font-weight title-table mt-4">
+            Наличие аналогов (заменителей) для запрошенного артикула на
+            центральном складе Кореана
+          </h4>
+          <Table data={data} columns={columns} />
         </div>
-        <h4 className="font-weight title-table">
-          Наличие для запрошенного артикула на центральном складе Кореана
-        </h4>
-        <Table data={data} columns={columns} />
-        <h4 className="font-weight title-table mt-4">
-          Наличие аналогов (заменителей) для запрошенного артикула на
-          центральном складе Кореана
-        </h4>
-        <Table data={data} columns={columns} />
+        <div className="Product-column--right">здесь колонка</div>
       </div>
     </div>
   );
